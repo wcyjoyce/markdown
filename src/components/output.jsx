1e4e1ed5629baba319d96f87aa723a9647ca7d43
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-26 02:12:42 +0800

    emoji support enabled

--- src/components/output.jsx
+++ src/components/output.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import ReactMarkdown from "react-markdown";
+import emoji from "emoji-dictionary";
 
 import Subheader from "./subheader.jsx";
 
 class Output extends Component {
   render() {
+    const emojiSupport = text => text.value.replace(/:\w+:/gi, name => emoji.getUnicode(name));
+
     return (
       <div className="half">
         <Subheader title="preview" />
         <div className="output content">
-          <ReactMarkdown source={this.props.content} />
+          <ReactMarkdown source={this.props.content} renderers={{ text: emojiSupport }} />
         </div>
       </div>
     );
